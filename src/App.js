a1ecb338d5a2cd5886e7525021afc256b4090c2a
Manogna-Vemu <manognavemu@gmail.com>
2024-07-25 10:46:53 +0530

    TestCommut

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
-function App(){
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
+import Tabs from "./Fifteen";
+
+function App(){
+return(
+    <BrowseRouter>
+        <Router>
+        <Route path="/" element={Tabs}/>
+        </Router>
+    </BrowseRouter>
+);
 }
 export default App;
